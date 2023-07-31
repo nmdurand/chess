f2e6117ad4a59cafd50d021fb9e201d38c4ca807
Nicolas Durand <nmdurand@gmail.com>
2023-07-31 23:57:40 +0200

    Enforces pieces movements

--- components/Square.tsx
+++ components/Square.tsx
@@ -1,5 +1,5 @@
 import { FC, useContext } from 'react'
-import { ChessContext } from '@/context/context'
+import { ChessContext, PieceColor, PieceName } from '@/context/context'
 import { Piece } from './Piece'
 import { useDroppable } from '@dnd-kit/core'
 
@@ -10,11 +10,53 @@ interface ISquare {
 
 export const Square: FC<ISquare> = ({ row, col }) => {
   const { state } = useContext(ChessContext)
-  const { board } = state
+  const { board, touchedPiece } = state
+  const { name, color } = board[row][col] || {}
+
+  const isDroppable = (() => {
+    if (!touchedPiece) return false
+    const {
+      name: touchedName,
+      color: touchedColor,
+      row: fromRow,
+      col: fromCol,
+    } = touchedPiece
+    switch (touchedName) {
+      case PieceName.Pawn:
+        return (
+          (touchedColor === PieceColor.White &&
+            col === fromCol &&
+            (row === fromRow + 1 || (fromRow === 1 && row === 3))) ||
+          (touchedColor === PieceColor.Black &&
+            col === fromCol &&
+            (row === fromRow - 1 || (fromRow === 6 && row === 4)))
+        )
+      case PieceName.Rook:
+        return row === fromRow || col === fromCol
+      case PieceName.Knight:
+        return (
+          (Math.abs(row - fromRow) === 2 && Math.abs(col - fromCol) === 1) ||
+          (Math.abs(row - fromRow) === 1 && Math.abs(col - fromCol) === 2)
+        )
+      case PieceName.Bishop:
+        return Math.abs(row - fromRow) === Math.abs(col - fromCol)
+      case PieceName.Queen:
+        return (
+          row === fromRow ||
+          col === fromCol ||
+          Math.abs(row - fromRow) === Math.abs(col - fromCol)
+        )
+      case PieceName.King:
+        return Math.abs(row - fromRow) <= 1 && Math.abs(col - fromCol) <= 1
+      default:
+        return false
+    }
+  })()
+
   const { isOver, setNodeRef } = useDroppable({
     id: `square-${row}-${col}`,
+    disabled: !isDroppable,
   })
-  const { name, color } = board[row][col] || {}
 
   return (
     <div
